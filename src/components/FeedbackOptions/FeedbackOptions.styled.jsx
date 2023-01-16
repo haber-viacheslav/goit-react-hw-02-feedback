0b7f8cb4a76bc6700c31d1273ee10f75b5c4fac9
Haber Viacheslav <gaberslavik@gmail.com>
2023-01-16 12:28:07 +0200

    finished styling

--- src/components/FeedbackOptions/FeedbackOptions.styled.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.styled.jsx
@@ -13,5 +13,13 @@ export const FeedbackOptionsButton = styled.ul`
   border-radius: 12px;
   background: linear-gradient(145deg, #0d013e, #2711ba);
   box-shadow: 2px 6px 8px #2e2bc2, -2px -2px 6px #1d06eb;
+  transition: 250ms transform ease-in-out, 300ms box-shadow ease-out,
+    150ms color ease-in;
   cursor: pointer;
+  &:hover,
+  &:focus {
+    color: #ffbb00;
+    transform: scale(1.1);
+    box-shadow: 2px 6px 8px #b8c501, -2px -2px 6px #fbff00;
+  }
 `;
